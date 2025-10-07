e87dc5dfe22d197576001299b77d0b0385adcc8b
Walther <walther.trgovac@outlook.com>
2025-10-07 17:14:41 +0200

    init

---
+++ src/franzmq/data_contracts/prekit/__init__.py
@@ -0,0 +1,9 @@
+from .payload import (
+    DataTag, DataTags, SignalData, 
+    DataTagContext, DataTagContexts, ServiceDetails, 
+    Computation, ComputationContext, ComputationContexts, 
+    ArgMapping, ValidationResults)
+
+__all__ = ["DataTag", "DataTags", "SignalData", "DataTagContext", "DataTagContexts",
+           "ServiceDetails", "Computation", "ComputationContext", "ComputationContexts", 
+           "ArgMapping", "ValidationResults"]
