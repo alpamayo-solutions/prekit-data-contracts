7284d6f596cccb38eba036ec8d8d5df42f3e5602
Till Schöpe <schoepe@alpamayo.ch>
2025-10-08 11:05:44 +0200

    monkey patch enum json encoding

--- src/franzmq/data_contracts/prekit/__init__.py
+++ src/franzmq/data_contracts/prekit/__init__.py
@@ -7,3 +7,11 @@ from .payload import (
 __all__ = ["DataTag", "DataTags", "SignalData", "DataTagContext", "DataTagContexts",
            "ServiceDetails", "Computation", "ComputationContext", "ComputationContexts", 
            "ArgMapping", "ValidationResults"]
+
+# Monkey Patching JSONEncoder
+_original_encoder = json.JSONEncoder.default
+def _patched_encoder(self, o):
+    if isinstance(o, enum.Enum):
+        return str(o)
+    return _original_encoder(self, o)
+json.JSONEncoder.default = _patched_encoder
